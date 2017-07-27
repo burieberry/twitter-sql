93cbc59ee87faae8d1138ed108f5fd09caafb7d2
Burcu Noyan <burcunoyan@gmail.com>
2017-07-26 23:34:37 -0400

    comment out tweetbank references in Routes; create client in /db

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use('/', function(req, res, next) {
 
 // Fallback for all page requests
 app.use(function(req, res, next) {
-  res.send('You idiot. This page does not exist.');
+  res.status(404).send('You idiot. This page does not exist.');
 });
 
 
